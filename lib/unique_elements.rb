ca7c0a24909eef87eb9d7f2aaa70bf02c44dd9d4
Nikki <nicleemart@gmail.com>
2016-06-16 00:19:59 +0000

    Submitting Ruby unique elements

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -1,6 +1,25 @@
-# This method takes an array of elements (these might be strings, integers, floats, or a
-# combination of the above), and returns an array of the elements that appear once and only
-# once.
+# For each value in the array that has a match, remove that value.
+
 def find_unique_elements(arr)
-  
+	has_match = 0
+	x = 0
+	y = 0
+	for value in arr
+		y = 0
+		has_match = 0
+		for value2 in arr
+			if y == x
+			elsif value == value2
+				has_match = 1
+				arr.delete(value2)
+			end
+			y += 1
+		end
+		if has_match == 1
+			arr.delete(value)
+		else
+			x+=1
+		end
+	end
+	return arr
 end
